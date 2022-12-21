13af15bfbe2f77e6158689f1dcd817c486a98e99
lintang <lintangprayogo12@gmail.com>
2022-12-21 14:38:45 +0700

    rate widget

---
+++ app/src/main/java/com/example/shonenapp/presentation/component/RateWidget.kt
@@ -0,0 +1,59 @@
+package com.example.shonenapp.presentation.component
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.scale
+import androidx.compose.ui.graphics.drawscope.translate
+import androidx.compose.ui.graphics.vector.PathParser
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.shonenapp.R
+
+@Composable
+fun RateWidget() {
+    val starPathString = stringResource(id = R.string.star_path)
+    val starPath = remember {
+        PathParser().parsePathString(pathData = starPathString).toPath()
+    }
+    val startPathBounds = remember {
+        starPath.getBounds()
+    }
+    filledStar(starPath = starPath, starPathBound = startPathBounds)
+}
+
+@Composable
+fun filledStar(
+    starPath: Path,
+    starPathBound: Rect,
+    scaleFactor: Float = 1f
+) {
+
+    Canvas(modifier = Modifier.size(24.dp)) {
+        val canvasSize = this.size
+        scale(scaleFactor) {
+            val left = (canvasSize.width / 2f) - (starPathBound.width / 1.7f)
+            val top = (canvasSize.height / 2f) - (starPathBound.height / 1.7f)
+            translate(left = left, top = top) {
+                drawPath(
+                    path = starPath,
+                    color = Color.Yellow
+                )
+            }
+        }
+
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewRateWidget() {
+    RateWidget()
+}
