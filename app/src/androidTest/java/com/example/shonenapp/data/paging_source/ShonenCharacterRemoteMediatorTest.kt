b240a5be89b586c6249d6891c19758f662f4a631
lintang <lintangprayogo12@gmail.com>
2023-01-15 15:39:26 +0700

    remote mediator testing

---
+++ app/src/androidTest/java/com/example/shonenapp/data/paging_source/ShonenCharacterRemoteMediatorTest.kt
@@ -0,0 +1,103 @@
+package com.example.shonenapp.data.paging_source
+
+import androidx.paging.*
+import androidx.paging.RemoteMediator.MediatorResult
+import androidx.test.core.app.ApplicationProvider
+import com.example.shonenapp.data.local.dao.ShonenDataBase
+import com.example.shonenapp.data.remote.FakeShonenApi2
+import com.example.shonenapp.domain.model.ShonenCharacterEntry
+import junit.framework.TestCase.assertFalse
+import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class ShonenCharacterRemoteMediatorTest {
+    private lateinit var shonenApiService: FakeShonenApi2
+    private lateinit var shonenDataBase: ShonenDataBase
+
+    @Before
+    fun setup() {
+        shonenApiService = FakeShonenApi2()
+        shonenDataBase = ShonenDataBase.create(ApplicationProvider.getApplicationContext(), false)
+    }
+
+    @After
+    fun cleanUp() {
+        shonenDataBase.clearAllTables()
+    }
+
+    @OptIn(ExperimentalPagingApi::class)
+    @Test
+    fun refreshloadReturnSuccessWhenMoreDataPresent() {
+        runBlocking {
+            val remoteMediator = ShonenCharacterRemoteMediator(
+                shonenApiService = shonenApiService,
+                shonenDataBase = shonenDataBase
+            )
+
+            val pagingState = PagingState<Int, ShonenCharacterEntry>(
+                pages = listOf(),
+                anchorPosition = null,
+                config = PagingConfig(3),
+                leadingPlaceholderCount = 0
+            )
+
+            val result = remoteMediator.load(LoadType.REFRESH, pagingState)
+
+            assertTrue(result is MediatorResult.Success)
+            assertFalse((result as MediatorResult.Success).endOfPaginationReached)
+        }
+    }
+
+    @OptIn(ExperimentalPagingApi::class)
+    @Test
+    fun refreshLoadSuccessAndEndOfPaginationTrueWhenNoMoreData() {
+        runBlocking {
+            shonenApiService.clearData()
+            val remoteMediator = ShonenCharacterRemoteMediator(
+                shonenApiService = shonenApiService,
+                shonenDataBase = shonenDataBase
+            )
+
+            val pagingState = PagingState<Int, ShonenCharacterEntry>(
+                pages = listOf(),
+                anchorPosition = null,
+                config = PagingConfig(3),
+                leadingPlaceholderCount = 0
+            )
+
+            val result = remoteMediator.load(LoadType.REFRESH, pagingState)
+
+            assertTrue(result is MediatorResult.Success)
+            assertTrue((result as MediatorResult.Success).endOfPaginationReached)
+        }
+    }
+    @OptIn(ExperimentalPagingApi::class)
+    @Test
+    fun refreshLoadWhenError() {
+        runBlocking {
+            shonenApiService.addException()
+            val remoteMediator = ShonenCharacterRemoteMediator(
+                shonenApiService = shonenApiService,
+                shonenDataBase = shonenDataBase
+            )
+
+            val pagingState = PagingState<Int, ShonenCharacterEntry>(
+                pages = listOf(),
+                anchorPosition = null,
+                config = PagingConfig(3),
+                leadingPlaceholderCount = 0
+            )
+
+            val result = remoteMediator.load(LoadType.REFRESH, pagingState)
+
+            assertTrue(result is MediatorResult.Error)
+        }
+    }
+
+
+
+
+}
--- app/src/main/java/com/example/shonenapp/data/local/dao/ShonenDataBase.kt
+++ app/src/main/java/com/example/shonenapp/data/local/dao/ShonenDataBase.kt
@@ -1,6 +1,8 @@
 package com.example.shonenapp.data.local.dao
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.example.shonenapp.domain.model.ShonenCharacterEntity
@@ -11,4 +13,15 @@ import com.example.shonenapp.domain.model.ShonenCharacterRemoteKeysEntity
 abstract class ShonenDataBase : RoomDatabase() {
     abstract fun characterDao(): CharacterDao
     abstract fun remoteKeyDao():RemoteKeysDao
+
+    companion object{
+        fun create(context:Context,useInMemory:Boolean ):ShonenDataBase{
+            val dataBaseBuilder = if(useInMemory){
+                Room.inMemoryDatabaseBuilder(context,ShonenDataBase::class.java)
+            }else{
+                Room.databaseBuilder(context,ShonenDataBase::class.java,"test_database.db")
+            }
+            return dataBaseBuilder.fallbackToDestructiveMigration().build()
+        }
+    }
 }
